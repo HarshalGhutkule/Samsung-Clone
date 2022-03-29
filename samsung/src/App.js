c000f93e6201f3c746ffda2f7a877c1c77a506d4
riteshjk <96072567+riteshjk@users.noreply.github.com>
2022-03-30 00:21:21 +0530

    changes

---
+++ samsung/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Overview } from './components/Overview';
+
+function App() {
+  return (
+    <div className="App">
+      <Overview/>
+    </div>
+  );
+}
+
+export default App;
