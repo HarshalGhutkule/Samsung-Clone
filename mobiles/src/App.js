b0267bf58bce4c9ac5db8b658f41bfe28567ae9f
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-03-29 20:07:33 +0530

    mobile page

--- mobiles/src/App.js
+++ mobiles/src/App.js
@@ -1,10 +1,20 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Navbar } from './Component/Navbar/Navbar';
+import { WhatsNew } from './Component/What\'sNew/What\'sNew';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ReactCurousel } from './Component/Carousel/Carousel';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <ReactCurousel/>
+      <WhatsNew/>
+      <Routes>
+        <Route path='' element={""}/>
+      </Routes>
     </div>
   );
 }
---
+++ mobiles/src/Component/What'sNew/What'sNew.jsx
@@ -0,0 +1,70 @@
+
+
+import styled from "styled-components";
+
+//styled component for Navbar
+const Main = styled.div`
+  
+  & div:nth-child(1){
+      height:150px;
+      border:1px solid red;
+  }
+  & div h1{
+      font-family:SamsungSharpSans;
+      font-size:36px;
+      padding:5%;
+  }
+  & div:nth-child(2){
+      display:grid;
+      height:525px;
+      grid-template-columns: 46% 23% 23%;
+      grid-template-rows: 50% 50%;
+      grid-template-areas:
+        'a b c'
+        'a d e';
+        gap: 20px;
+      border:1px solid purple;
+  }
+  & div:nth-child(2) div{
+      background-color:#f4f4f4;
+      border-radius:10px;
+  }
+  & .mainbox{
+    grid-area: a;
+    border:1px solid red;
+  }
+  & .box1{
+    grid-area: b;
+    border:1px solid red;
+  }
+  & .box2{
+    grid-area: c;
+    border:1px solid red;
+  }
+  & .box3{
+    grid-area: d;
+    border:1px solid red;
+  }
+  & .box4{
+    grid-area: e;
+    border:1px solid red;
+  }
+`;
+
+
+export const WhatsNew = ()=>{
+    return <Main>
+        <div>
+            <h1>
+                What's New
+            </h1>
+        </div>
+        <div>
+            <div className="mainbox"></div>
+            <div className="box1"></div>
+            <div className="box2"></div>
+            <div className="box3"></div>
+            <div className="box4"></div>
+        </div>
+    </Main>
+}
