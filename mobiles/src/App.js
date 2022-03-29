c61f7e4e947fe2e776dfa4a723bb757f8877e378
Harshal Ghutkule <95927895+HarshalGhutkule@users.noreply.github.com>
2022-03-30 02:38:02 +0530

    mobile page

--- mobiles/src/App.js
+++ mobiles/src/App.js
@@ -14,11 +14,11 @@ function App() {
       <Navbar/>
       <ReactCurousel/>
       <WhatsNew/>
-      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2108/pcd/smp/pc/PCD_COMBO_KV_Main-KV_1440X640_LTR.jpg?$1440_640_JPG$"} title={"Galaxy Z"} description={"Unfold your world with the completely reimagined smartphone experience."} top={"1500px"} color={"black"} margin={"55%"}/>
-      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_Combo_KV_Curation-KV_1440x640_pc.jpg?$1440_640_JPG$"} title={"Galaxy S"} description={"Welcome to the epic standard"} top={"2120px"} color={"black"} margin={"10%"}/>
-      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/pcd/smartphones/04_galaxy_note_pc.jpg?$1440_640_JPG$"} title={"Galaxy Note"} description={"The Powerphone that empowers your work and play"} top={"2800px"} color={"white"} margin={"55%"}/>
-      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2203/pcd/PCD_KV_optC_PC_1440X640.jpg?$1440_640_JPG$"} title={"Galaxy A Series"} description={"Awesome is for everyone"} top={"3350px"} color={"black"} margin={"10%"}/>
-      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_Ecosystem_KV_Curation-KV_1440x640_pc.jpg?$1440_640_JPG$"} title={"Amazing Alone.Better Together"} description={"Awesome is for everyone"} top={"3950px"} color={"black"} margin={"10%"}/>
+      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2108/pcd/smp/pc/PCD_COMBO_KV_Main-KV_1440X640_LTR.jpg?$1440_640_JPG$"} title={"Galaxy Z"} description={"Unfold your world with the completely reimagined smartphone experience."} top={"1650px"} color={"black"} margin={"55%"}/>
+      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_Combo_KV_Curation-KV_1440x640_pc.jpg?$1440_640_JPG$"} title={"Galaxy S"} description={"Welcome to the epic standard"} top={"2270px"} color={"black"} margin={"10%"}/>
+      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/pcd/smartphones/04_galaxy_note_pc.jpg?$1440_640_JPG$"} title={"Galaxy Note"} description={"The Powerphone that empowers your work and play"} top={"2900px"} color={"white"} margin={"55%"}/>
+      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2203/pcd/PCD_KV_optC_PC_1440X640.jpg?$1440_640_JPG$"} title={"Galaxy A Series"} description={"Awesome is for everyone"} top={"3500px"} color={"black"} margin={"10%"}/>
+      <ImageLoaded image={"https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_Ecosystem_KV_Curation-KV_1440x640_pc.jpg?$1440_640_JPG$"} title={"Amazing Alone.Better Together"} description={"Awesome is for everyone"} top={"4100px"} color={"black"} margin={"10%"}/>
       <ScrollToTop />
       <Routes>
         <Route path='' element={""}/>
--- mobiles/src/Component/What'sNew/What'sNew.jsx
+++ mobiles/src/Component/What'sNew/What'sNew.jsx
@@ -1,69 +1,222 @@
-
-
 import styled from "styled-components";
 
 //styled component for Navbar
 const Main = styled.div`
-  
-  & div:nth-child(1){
-      height:150px;
-      border:1px solid purple;
+  & > div:nth-child(1) {
+    height: 150px;
   }
-  & div h1{
-      font-family:SamsungSharpSans;
-      font-size:36px;
-      padding:5%;
+  & div h1 {
+    font-family: SamsungSharpSans;
+    font-size: 36px;
+    padding: 5%;
   }
-  & div:nth-child(2){
-      height:525px;
-      display:grid;
-      grid-template-columns: 46% 24% 24%;
-      grid-template-rows: 250px 250px;
-      grid-template-areas:
-        'a b c'
-        'a d e';
-      border:1px solid purple;
+  & > div:nth-child(2) {
+    height: 700px;
+    display: grid;
+    grid-template-columns: 50% 25% 25%;
+    grid-template-rows: 340px 340px;
+    grid-template-areas:
+      "a b c"
+      "a d e";
   }
-  & div:nth-child(2) div{
-      background-color:#f4f4f4;
-      border-radius:10px;
+  & div:nth-child(2) div {
+    background-color: #f4f4f4;
+    border-radius: 20px;
   }
-  & div:nth-child(2) .mainbox{
+  & #mainbox {
     grid-area: a;
-    border:1px solid red;
+    margin: 2%;
   }
-  & div:nth-child(2) .box1{
+  & .box1 {
     grid-area: b;
-    border:1px solid red;
+    margin: 4%;
+    overflow: hidden;
+    cursor: pointer;
   }
-  & div:nth-child(2) .box2{
+  & .box1:hover > button {
+    top: -10%;
+  }
+  & .box1:hover > #forhover {
+    top: -5%;
+  }
+  & .box1:hover > img {
+    transform: scale(1.1);
+    transition: all .5s ease-out;
+  }
+  & .box1:hover > p:nth-child(8) {
+    top: 100%;
+    position: relative;
+    transition: all 0.5s ease-out;
+  }
+  & .box1 > p:nth-child(7) {
+    font-family: samsung700;
+    font-size: 17px;
+  }
+  & .box1 > p:nth-child(8) {
+    font-size: 12px;
+    font-family: samsung400;
+    top: 0;
+  }
+  & .box1 button {
+    top: 100%;
+    font-family: samsung400;
+    position: relative;
+    transition: all 0.5s ease-out;
+    height: 35px;
+    width: 100px;
+    background-color: black;
+    border-radius: 20px;
+    border: transparent;
+    color: white;
+    font-weight: 700;
+    font-size: 13px;
+    margin-left: 15px;
+  }
+  & Button:hover {
+    opacity: 75%;
+  }
+  & #subBox1 {
     grid-area: c;
-    border:1px solid red;
+    margin: 4%;
   }
-  & div:nth-child(2) .box3{
+  & #subBox2 {
     grid-area: d;
-    border:1px solid red;
+    margin: 4%;
   }
-  & div:nth-child(2) .box4{
+  & #subBox3 {
     grid-area: e;
-    border:1px solid red;
+    margin: 4%;
   }
 `;
 
-
-export const WhatsNew = ()=>{
-    return <Main>
-        <div>
-            <h1>
-                What's New
-            </h1>
+export const WhatsNew = () => {
+  return (
+    <Main>
+      <div>
+        <h1>What's New</h1>
+      </div>
+      <div>
+        <div className="box1" id="mainbox">
+        <span
+            class="w3-badge w3-blue"
+            style={{
+              borderRadius: "15px",
+              fontSize: "12px",
+              float: "left",
+              marginLeft: "5%",
+              marginTop: "5%",
+            }}
+          >
+            New
+          </span>
+          <br />
+          <br />
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_B_KV_Merchandising_376x376_.png?$376_376_PNG$"
+            alt="png"
+            style={{marginTop:"10%", marginLeft:"-10%"}}
+          />
+          <br />
+          <br />
+          <p style={{fontSize:"23px",marginTop:"2%"}}>Galaxy S22 Ultra</p>
+          <p style={{fontSize:"14px"}}>Just launched.</p>
+          <button id="forhover">Learn more</button>
         </div>
-        <div>
-            <div className="mainbox"></div>
-            <div className="box1"></div>
-            <div className="box2"></div>
-            <div className="box3"></div>
-            <div className="box4"></div>
+        <div className="box1">
+          <span
+            class="w3-badge w3-blue"
+            style={{
+              borderRadius: "15px",
+              fontSize: "12px",
+              float: "left",
+              marginLeft: "5%",
+              marginTop: "5%",
+            }}
+          >
+            New
+          </span>
+          <br />
+          <br />
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/PCD_RG_KV_Merchandising_160X160_pc.png?$160_160_PNG$"
+            alt="png"
+          />
+          <br />
+          <br />
+          <p>Galaxy S22 | S22+</p>
+          <p>Just launched.</p>
+          <button>Learn more</button>
         </div>
+        <div className="box1" id="subBox1">
+          <span
+            class="w3-badge w3-blue"
+            style={{
+              borderRadius: "15px",
+              fontSize: "12px",
+              float: "left",
+              marginLeft: "5%",
+              marginTop: "5%",
+            }}
+          >
+            New
+          </span><br /><br />
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/2203/pcd/PCD_Product-Tile_PC_160X160.png?$160_160_PNG$"
+            alt="png"
+          />
+          <br />
+          <br />
+          <p>Galaxy A53 5G</p>
+          <p>.</p>
+          <button>Learn more</button>
+        </div>
+        <div className="box1" id="subBox2">
+        <span
+            class="w3-badge w3-blue"
+            style={{
+              borderRadius: "15px",
+              fontSize: "12px",
+              float: "left",
+              marginLeft: "5%",
+              marginTop: "5%",
+            }}
+          >
+            New
+          </span><br /><br />
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/2202/pcd/smp/Q2_KV_Merchandising_160X160.png?$160_160_PNG$"
+            alt="png"
+          />
+          <br />
+          <br />
+          <p>Galaxy Z Fold3 5G</p>
+          <p>.</p>
+          <button>Learn more</button>
+        </div>
+        <div className="box1" id="subBox3">
+        <span
+            class="w3-badge w3-blue"
+            style={{
+              borderRadius: "15px",
+              fontSize: "12px",
+              float: "left",
+              marginLeft: "5%",
+              marginTop: "5%",
+            }}
+          >
+            New
+          </span><br /><br />
+          <img
+            src="https://images.samsung.com/is/image/samsung/assets/in/2108/pcd/smp/pc/PCD_B2_KV_Merchandising_160X160.png?$160_160_PNG$"
+            alt="png"
+          />
+          <br />
+          <br />
+          <p>Galaxy Z Flip3 5G</p>
+          <p>.</p>
+          <button>Learn more</button>
+        </div>
+      </div>
     </Main>
-}
+  );
+};
